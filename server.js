71f8d150a9eb6ad9b9396eecbc4b5121a780e5ba
student-navyasharma <navyasharma01k@gmail.com>
2026-03-26 12:02:42 +0530

    updated backend

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const cors=require('cors')
 const app=express()
 app.use(cors())
 app.use(express.json())
+
+let users={}
+
 app.get('/',(req,res)=>{
     res.send('Server is running')
 })
@@ -15,6 +18,49 @@ app.post('/login',(req,res)=>{
 
     // Temporary response
     res.json({ message: "Login received successfully" })
+
+    const useragent=req.headers['user-agent']
+    console.log("User Agent:", useragent)
+
+    let device="Desktop"
+    if(useragent.toLowerCase().includes("mobile")){
+        device="Mobile"
+    }
+    console.log("Device Type:", device)
+
+
+   let browser = "Unknown"
+
+// Check specific browsers first
+if (useragent.includes("Edg")) browser = "Edge"
+else if (useragent.includes("Chrome")) browser = "Chrome"
+else if (useragent.includes("Firefox")) browser = "Firefox"
+else if (useragent.includes("Safari")) browser = "Safari"
+
+console.log("Browser:", browser)
+
+
+
+
+
+
+ if(users[email]){
+        const lastDevice=users[email].device
+        const lastBrowser=users[email].browser
+
+        if(lastDevice!==device || lastBrowser!==browser){
+            console.log("Suspicious login detected for user:", email)
+        }else{
+            console.log("Login from known device and browser for user:", email)
+        }
+    }else{
+        console.log("New user login detected:", email)
+    }
+
+    users[email]={
+        device:device,
+        browser:browser 
+    }
 })
 
 app.listen(5000,()=>{
@@ -45,7 +91,23 @@ app.listen(5000,()=>{
 
 // Start server on port 5000 → backend starts running and listens for requests
 
-
-
-
 // This server receives login data from frontend, processes it, and sends a response back
+
+
+
+
+ // Get user-agent string from request headers (info sent automatically by browser)
+
+ // User-agent contains details about user's device, browser, and OS
+
+ // Detect device type (Mobile or Desktop) by checking "mobile" keyword
+
+ // Detect browser (Chrome, Firefox, Safari) by checking keywords in user-agent
+
+ // Print detected device and browser in terminal for verification
+
+
+
+ // Edge user-agent also contains "Chrome"
+// So we must check "Edg" before "Chrome"
+// Otherwise Edge will be wrongly detected as Chrome
